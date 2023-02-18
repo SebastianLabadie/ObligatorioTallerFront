94f53cdf66f2553782c7853c38b33648b551ed65
RodrigoPensatori <rodrigo.pensatori@hotmail.com>
2023-02-18 19:19:36 -0300

    a

--- src/routes/Registro.jsx
+++ src/routes/Registro.jsx
@@ -4,6 +4,8 @@ import { setMenuActivo } from "../features/menuSlice";
 import { URL_BASE } from "../utils/utils";
 import axios from "axios";
 import Select from 'react-select';
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 
 export default function Registro() {
 	const dispatch = useDispatch();
@@ -75,16 +77,20 @@ export default function Registro() {
 				"idDepartamento": Departamento,
 				"idCiudades":Ciudad,
 			}
-			const res = await axios.post(`${URL_BASE}usuarios.php`)
+			const res = await axios.post(`${URL_BASE}usuarios.php`,req)
 			console.log(res.data);
 			if(res.data.codigo ===200){
-				alert("Registro realizado con exito")
+				toast.success("Usuario Registrado con exito")
+				setUsuario("")
+				setPassword("");
+				setCiudad(0);
+				setDepartamento(0);
 			}
 			else{
-				alert(`ERROR : ${res.data.codigo}`)
+				toast.error(res.data.mensaje)
 			}
 		} catch (error) {
-			
+			toast.error(error.response.data.mensaje)
 		}
 	}
 
@@ -110,6 +116,7 @@ export default function Registro() {
 									placeholder="Pepe_1234"
 									required=""
 									onChange={handleChangeUsuario}
+									value={usuario}
 								/>
 							</div>
 							<div>
@@ -124,6 +131,7 @@ export default function Registro() {
 									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									required=""
 									onChange={handleChangePassword}
+									value={password}
 								/>
 							</div>
 								<label htmlFor="Departamento" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -155,7 +163,7 @@ export default function Registro() {
 							</div>
 
 							<button
-								type="submit"
+								type="button"
 								className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
 								onClick={()=> RegistrarUsr()}
 							>
@@ -163,9 +171,9 @@ export default function Registro() {
 							</button>
 							<p className="text-sm font-light text-gray-500 dark:text-gray-400">
 								Tienes una cuenta?{" "}
-								<a href="/" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+								<Link to={"/"} className="font-medium text-primary-600 hover:underline dark:text-primary-500">
 									Inicia Sesión
-								</a>
+								</Link>
 							</p>
 						</form>
 					</div>
