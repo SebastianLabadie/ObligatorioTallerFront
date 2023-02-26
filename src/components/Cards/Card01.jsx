b77aeddff0a152b30798feda636bde74a451784b
Sebastian LB <sebastianlabadie.contact@gmail.com>
2023-02-26 17:52:48 -0300

    grafico gastos evolutivos

--- src/components/Cards/Card01.jsx
+++ src/components/Cards/Card01.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Card01({title, description, children}) {
+export default function Card01({title, description,maxWidth='max-w-sm', children}) {
 	return (
 		<div className="flex justify-center mx-3">
-			<div className="block max-w-sm rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-700">
+			<div className={`block ${maxWidth} rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-700`}>
 				<h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">{title}</h5>
 				<p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
 					{description}
--- src/routes/Analisis.jsx
+++ src/routes/Analisis.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import BackGroundMain from "../components/BackgroundMain";
 import Card01 from "../components/Cards/Card01";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, CategoryScale, LinearScale, BarElement, Title, Legend } from "chart.js";
+import { Pie, Bar } from "react-chartjs-2";
 import { useSelector } from "react-redux";
 import Spinner from "../components/Spinner";
 import { URL_BASE } from "../utils/utils";
 import axios from "axios";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, Title);
 
 const arrayColores = [
 	"rgba(255, 99, 132, 0.2)",
@@ -19,6 +19,40 @@ const arrayColores = [
 	"rgba(255, 159, 64, 0.2)",
 ];
 
+const arrayMeses = [
+	{ value: 1, label: "Enero" },
+	{ value: 2, label: "Febrero" },
+	{ value: 3, label: "Marzo" },
+	{ value: 4, label: "Abril" },
+	{ value: 5, label: "Mayo" },
+	{ value: 6, label: "Junio" },
+	{ value: 7, label: "Julio" },
+	{ value: 8, label: "Agosto" },
+	{ value: 9, label: "Septiembre" },
+	{ value: 10, label: "Octubre" },
+	{ value: 11, label: "Noviembre" },
+	{ value: 12, label: "Diciembre" },
+];
+
+const options = {
+	indexAxis: "y",
+	elements: {
+		bar: {
+			borderWidth: 2,
+		},
+	},
+	responsive: true,
+	plugins: {
+		legend: {
+			position: "right",
+		},
+		title: {
+			display: false,
+			text: "",
+		},
+	},
+};
+
 export default function Analisis() {
 	const { userData } = useSelector((state) => state.usuario);
 	const [loading, setLoading] = useState(true);
@@ -27,6 +61,7 @@ export default function Analisis() {
 	const [dataGastosRubros, setDataGastosRubros] = useState({});
 	const [totalGastos, setTotalGastos] = useState(0);
 	const [rubros, setRubros] = useState([]);
+	const [dataGastosEvolucion, setDataGastosEvolucion] = useState({});
 
 	useEffect(() => {
 		console.log(userData.id);
@@ -35,7 +70,7 @@ export default function Analisis() {
 
 	useEffect(() => {
 		getMovimientos();
-	},[rubros])
+	}, [rubros]);
 
 	const getRubros = async () => {
 		console.log(`axios ${axios.defaults.headers.common["apiKey"]}`);
@@ -61,10 +96,9 @@ export default function Analisis() {
 			console.log(`${URL_BASE}movimientos.php?idUsuario=${userData.id}`);
 			const res = await axios.get(`${URL_BASE}movimientos.php?idUsuario=${userData.id}`);
 			console.log(res.data);
-			await calcularChartIngresoRubros(res)
-			await calcularChartGastosRubros(res)
-
-			
+			await calcularChartIngresoRubros(res);
+			await calcularChartGastosRubros(res);
+			calcularChartGastosEvolicion(res);
 		} catch (error) {
 			console.log(error);
 		} finally {
@@ -73,10 +107,9 @@ export default function Analisis() {
 	};
 
 	const calcularChartIngresoRubros = async (res) => {
-		console.log(`rubros `, rubros)
+		console.log(`rubros `, rubros);
 		const rubrosIngreso = rubros.filter((item) => item.tipo === "ingreso");
 		const rubrosIngresoLabel = rubrosIngreso.map((item) => item.nombre);
-		
 
 		let movimientosIngresoRubros = [];
 		let totalIngresos = 0;
@@ -89,7 +122,6 @@ export default function Analisis() {
 			totalIngresos += totalMovimientosDeRubro;
 			movimientosIngresoRubros.push(totalMovimientosDeRubro);
 		});
-		
 
 		setDataIngresoRubros({
 			labels: rubrosIngresoLabel,
@@ -108,10 +140,9 @@ export default function Analisis() {
 	};
 
 	const calcularChartGastosRubros = async (res) => {
-		console.log(`rubros `, rubros)
+		console.log(`rubros `, rubros);
 		const rubrosGastos = rubros.filter((item) => item.tipo === "gasto");
 		const rubrosGastosLabel = rubrosGastos.map((item) => item.nombre);
-		
 
 		let movimientosGastosRubros = [];
 		let totalGastos = 0;
@@ -124,7 +155,6 @@ export default function Analisis() {
 			totalGastos += totalMovimientosDeRubro;
 			movimientosGastosRubros.push(totalMovimientosDeRubro);
 		});
-		
 
 		setDataGastosRubros({
 			labels: rubrosGastosLabel,
@@ -142,8 +172,69 @@ export default function Analisis() {
 		setTotalGastos(totalGastos);
 	};
 
+	const calcularChartGastosEvolicion = (res) => {
+		const rubrosGastos = rubros.filter((item) => item.tipo === "gasto").map((item) => item.id);
+
+		console.log(`rubrosGastos `, rubrosGastos);
+
+		const movimientosGastos = res.data.movimientos.filter((movimiento) => rubrosGastos.includes(movimiento.categoria));
+
+		console.log(`movimientosGastos `, movimientosGastos);
+
+		const date = new Date();
+		const anioActual = date.getFullYear();
+		const anioPasado = anioActual - 1;
+
+		const movimientosGastosPorMesAActual = [];
+		arrayMeses.forEach((mes, index) => {
+			const movimientosDeMes = movimientosGastos.filter((movimiento) => {
+				const fecha = new Date(movimiento.fecha);
+				//console.log(`month ${fecha.getMonth()} mes.value ${mes.value} year ${fecha.getFullYear()} anioActual ${anioActual}  `)
+				return fecha.getMonth() + 1 == mes.value && fecha.getFullYear() == anioActual;
+			});
+			const totalMovimientosDeMes = movimientosDeMes.reduce((acumulador, movimiento) => acumulador + movimiento.total, 0);
+			movimientosGastosPorMesAActual.push(totalMovimientosDeMes);
+		});
+
+		const movimientosGastosPorMesAPasado = [];
+
+		arrayMeses.forEach((mes, index) => {
+			const movimientosDeMes = movimientosGastos.filter((movimiento) => {
+				const fecha = new Date(movimiento.fecha);
+				//console.log(`month ${fecha.getMonth()} mes.value ${mes.value} year ${fecha.getFullYear()} anioActual ${anioActual}  `)
+				return fecha.getMonth() + 1 == mes.value && fecha.getFullYear() == anioPasado;
+			});
+			const totalMovimientosDeMes = movimientosDeMes.reduce((acumulador, movimiento) => acumulador + movimiento.total, 0);
+			movimientosGastosPorMesAPasado.push(totalMovimientosDeMes);
+		});
+
+		console.log(`movimientosGastosPorMesAActual ${movimientosGastosPorMesAActual}`);
+		console.log(`movimientosGastosPorMesAPasado ${movimientosGastosPorMesAPasado}`);
+
+		const labels = arrayMeses.map((item) => item.label);
+
+		setDataGastosEvolucion({
+			labels,
+			datasets: [
+				{
+					label: "Gastos " + anioActual,
+					data: labels.map((el,idx)=>movimientosGastosPorMesAActual[idx]),
+					borderColor: "rgb(255, 99, 132)",
+					backgroundColor: "rgba(255, 99, 132, 0.5)",
+				},
+				{
+					label: "Gastos " + anioPasado,
+					data: labels.map((el,idx)=>movimientosGastosPorMesAPasado[idx]),
+					borderColor: "rgb(53, 162, 235)",
+					backgroundColor: "rgba(53, 162, 235, 0.5)",
+				},
+			],
+		});
+	};
+
 	return (
-		<div className="bg-slate-100  h-screen">
+		<div className="bg-slate-100 ">
+			
 			{loading && <Spinner />}
 			{!loading && (
 				<div className="p-5">
@@ -161,6 +252,12 @@ export default function Analisis() {
 						>
 							<Pie data={dataGastosRubros} />
 							<strong>Total: ${totalGastos}</strong>
+						</Card01>
+						
+					</div>
+					<div className="flex justify-center items-center mt-5">
+						<Card01 title={"Gastos evolutivos"} description={`Grafico de gastos por mes para el usuario para el año en curso y el anterior.`}  maxWidth="max-w-800">
+								<Bar options={options} data={dataGastosEvolucion} />
 						</Card01>
 					</div>
 				</div>
