ca1caa0b6cda56649b41773fbbc5d7624d991379
Sebastian LB <sebastianlabadie.contact@gmail.com>
2023-02-26 18:46:21 -0300

    login disabled button

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -186,6 +186,7 @@ export default function Login() {
 							<div className="text-center lg:text-left">
 								<button
 									type="button"
+									disabled={!usuario || !password}
 									onClick={handleClickLogin}
 									className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
 								>
